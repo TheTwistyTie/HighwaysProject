cb7515bc123d52eed9ead9d6dab8f47c4bda49f2
thetwistytie <alexander.n.keith@gmail.com>
2019-02-19 20:40:25 -0500

    2/19 8:40

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,12 +6,23 @@ using BansheeGz.BGSpline.Curve;
 public class GameManager : MonoBehaviour
 {
 
+    public GameObject ActivePath;
+
+    private void Update()
+    {
+        if ((Input.GetKeyDown(KeyCode.LeftControl) || Input.GetKeyDown(KeyCode.RightControl)) && Input.GetMouseButtonDown(0))
+        {
+            CreateNewPath(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        }
+    }
+
     List<Path> paths = new List<Path>();
 
     void CreateNewPath(Vector3 pos)
     {
 
-        paths.Add(new Path(pos));
+        paths.Add(new Path(pos, paths.Count));
+        paths[paths.Count - 1].SetParent(transform);
 
     }
 
--- Assets/Scripts/Path.cs
+++ Assets/Scripts/Path.cs
@@ -1,20 +1,45 @@
 ﻿using System;
 using UnityEngine;
 using BansheeGz.BGSpline.Curve;
+using BansheeGz.BGSpline.Components;
 
-public struct Path
+public class Path
 {
 
     public GameObject gameObject;
     public BGCurve curve;
+    public BGCcCollider3DBox collider;
+    public BGCcVisualizationLineRenderer renderer;
+    public PathController controller;
 
-    public Path(Vector3 pos)
+    public int index;
+
+    public Path(Vector3 pos, int index)
     {
+
+        this.index = index;
+
         gameObject = new GameObject();
         gameObject.transform.position = pos;
+
         gameObject.AddComponent<BGCurve>();
         curve = gameObject.GetComponent<BGCurve>();
+
+        gameObject.AddComponent<BGCcCollider3DBox>();
+        collider = gameObject.GetComponent<BGCcCollider3DBox>();
+
+        gameObject.AddComponent<BGCcVisualizationLineRenderer>();
+        renderer = gameObject.AddComponent<BGCcVisualizationLineRenderer>();
+
+        gameObject.AddComponent<PathController>();
+        controller = gameObject.GetComponent<PathController>();
+        controller.path = this;
+
     }
 
+    public void SetParent(Transform t)
+    {
+        gameObject.transform.SetParent(t);
+    }
 }
 
